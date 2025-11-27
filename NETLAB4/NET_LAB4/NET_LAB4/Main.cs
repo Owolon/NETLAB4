47760c8cd28261bd8919c354eff015e2b329cd1a
Owolon <keepoutmind@gmail.com>
2025-11-27 11:23:35 +0300

    Add files via upload

---
+++ NETLAB4/NET_LAB4/NET_LAB4/Main.cs
@@ -0,0 +1,283 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+
+namespace NET_LAB4
+{
+    class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("\n1 - Формирование массивов");
+                Console.WriteLine("2 - Среднеарифметические двух массивов");
+                Console.WriteLine("3 - Сумма элементов главной диагонали");
+                Console.WriteLine("4 - Быстрая сортировка массива");
+                Console.WriteLine("0 - Выход");
+                Console.Write("Введите номер: ");
+                string choice = Console.ReadLine();
+
+                if (choice == "0")
+                {
+                    break;
+                }
+
+                switch (choice)
+                {
+                    case "1":
+                        Task1();
+                        break;
+                    case "2":
+                        Task2();
+                        break;
+                    case "3":
+                        Task3();
+                        break;
+                    case "4":
+                        Task4();
+                        break;
+                    default:
+                        Console.WriteLine("Некорректный выбор. Попробуйте еще раз.");
+                        break;
+                }
+            }
+        }
+
+        static void Task1()
+        {
+            try
+            {
+                Console.Write("Введите значение n (длина массива): ");
+                int n = int.Parse(Console.ReadLine());
+                Console.Write("Введите значение m (количество столбцов): ");
+                int m = int.Parse(Console.ReadLine());
+                if (n <= 0 || m <= 0)
+                {
+                    Console.WriteLine("Значения n и m должны быть положительными.");
+                    return;
+                }
+
+                Random rand = new Random();
+
+                double[] array1D = new double[n];
+                double[,] array2D = new double[n, m];
+
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < m; j++)
+                    {
+                        int k = rand.Next(1, 10);
+                        Console.Write("\nСлучайное значение K: {0} ", k);
+                        double value = Math.Pow(5, k) / Math.Sqrt((i+1) * ((i+1) + 1));
+
+                        array1D[i] = value;
+                        array2D[i, j] = value;
+                    }
+                }
+
+                Console.WriteLine("\nОдномерный массив:");
+                foreach (var item in array1D)
+                {
+                    Console.WriteLine($"{item:F1}");
+                }
+
+                Console.WriteLine("Двумерный массив:");
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < m; j++)
+                    {
+                        Console.Write($"{array2D[i, j]:F1} ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число.");
+            }
+        }
+
+        static void Task2()
+        {
+            int sizeA, sizeB;
+
+            Console.Write("Введите размер массива A: ");
+            if (!int.TryParse(Console.ReadLine(), out sizeA) || sizeA <= 0)
+            {
+                Console.WriteLine("Некорректный ввод размера A.");
+                return;
+            }
+
+            Console.Write("Введите размер массива B: ");
+            if (!int.TryParse(Console.ReadLine(), out sizeB) || sizeB <= 0)
+            {
+                Console.WriteLine("Некорректный ввод размера B.");
+                return;
+            }
+
+            //Рандоммм
+            double[] A = new double[sizeA];
+            double[] B = new double[sizeB];
+            Random rand = new Random();
+
+            for (int i = 0; i < sizeA; i++)
+            {
+                A[i] = rand.NextDouble() * 100;
+            }
+            for (int i = 0; i < sizeB; i++)
+            {
+                B[i] = rand.NextDouble() * 100;
+            }
+
+            double sumA = 0;
+            foreach (var item in A)
+            {
+                sumA += item;
+            }
+            double meanA = sumA / A.Length;
+
+            double sumB = 0;
+            foreach (var item in B)
+            {
+                sumB += item;
+            }
+            double meanB = sumB / B.Length;
+
+            Console.WriteLine("Массив A:");
+            foreach (var item in A)
+            {
+                Console.Write($"{item:F2} ");
+            }
+            Console.WriteLine($"\nСреднее A: {meanA:F1}");
+
+            Console.WriteLine("Массив B:");
+            foreach (var item in B)
+            {
+                Console.Write($"{item:F1} ");
+            }
+            Console.WriteLine($"\nСреднее B: {meanB:F1}");
+        }
+
+        static void Task3()
+        {
+            try
+            {
+                Console.Write("Введите размер массива n: ");
+                int n = int.Parse(Console.ReadLine());
+                if (n <= 0)
+                {
+                    Console.WriteLine("Размер массива доджен быть положительным числом.");
+                    return; 
+                }
+
+                double[,] A = new double[n, n];
+                Random rand = new Random();
+
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        A[i, j] = rand.NextDouble() * 100;
+                    }
+                }
+
+                Console.WriteLine("Массив A:");
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        Console.Write($"{A[i, j]:F1} ");
+                    }
+                    Console.WriteLine();
+                }
+
+                double sumDiagonal = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    sumDiagonal += A[i, i];
+                }
+                Console.WriteLine($"Сумма элементов главной диагонали: {sumDiagonal:F1}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число.");
+            }
+        }
+
+        static void Task4()
+        {
+            try
+            {
+                Console.Write("Введите размер массива для сортировки: ");
+                int size = int.Parse(Console.ReadLine());
+                if (size <= 0)
+                {
+                    Console.WriteLine("Размер должен быть положительным.");
+                    return;
+                }
+
+                double[] array = new double[size];
+                Random rand = new Random();
+
+                for (int i = 0; i < size; i++)
+                {
+                    array[i] = rand.NextDouble() * 100;
+                }
+
+                Console.WriteLine("Массив до сортировки:");
+                foreach (var item in array)
+                {
+                    Console.Write($"{item:F2} ");
+                }
+                Console.WriteLine();
+
+                QuickSort(array, 0, array.Length - 1);
+
+                Console.WriteLine("Массив после сортировки:");
+                foreach (var item in array)
+                {
+                    Console.Write($"{item:F2} ");
+                }
+                Console.WriteLine();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число.");
+            }
+        }
+
+        static void QuickSort(double[] arr, int left, int right)
+        {
+            if (left >= right)
+                return;
+
+            double pivot = arr[(left + right) / 2];
+            int i = left;
+            int j = right;
+            while (i <= j)
+            {
+                while (arr[i] < pivot) 
+                    i++;
+                while (arr[j] > pivot) 
+                    j--;
+                if (i <= j)
+                {
+                    double temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                    i++;
+                    j--;
+                }
+            }
+
+            if (left < j) 
+                QuickSort(arr, left, j);
+            if (i < right) 
+                QuickSort(arr, i, right);
+        }
+    }
+
+}
---
+++ NETLAB4/NET_LAB4W/NET_LAB4W/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace NET_LAB4W
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            textBoxPut0 = new TextBox();
+            buttonClear = new Button();
+            textBoxOutput = new TextBox();
+            buttonAdd = new Button();
+            textBoxPut1 = new TextBox();
+            textBoxPut2 = new TextBox();
+            textBoxPut3 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            groupBox1 = new GroupBox();
+            errorProvider1 = new ErrorProvider(components);
+            errorProvider2 = new ErrorProvider(components);
+            errorProvider3 = new ErrorProvider(components);
+            errorProvider4 = new ErrorProvider(components);
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)errorProvider2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)errorProvider3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)errorProvider4).BeginInit();
+            SuspendLayout();
+            // 
+            // textBoxPut0
+            // 
+            textBoxPut0.Location = new Point(1, 46);
+            textBoxPut0.Name = "textBoxPut0";
+            textBoxPut0.Size = new Size(340, 23);
+            textBoxPut0.TabIndex = 1;
+            // 
+            // buttonClear
+            // 
+            buttonClear.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            buttonClear.Location = new Point(517, 361);
+            buttonClear.Name = "buttonClear";
+            buttonClear.Size = new Size(134, 65);
+            buttonClear.TabIndex = 2;
+            buttonClear.Text = "Очистить";
+            buttonClear.UseVisualStyleBackColor = true;
+            buttonClear.Click += buttonClear_Click_1;
+            // 
+            // textBoxOutput
+            // 
+            textBoxOutput.BackColor = SystemColors.GradientActiveCaption;
+            textBoxOutput.BorderStyle = BorderStyle.None;
+            textBoxOutput.Location = new Point(30, 70);
+            textBoxOutput.Multiline = true;
+            textBoxOutput.Name = "textBoxOutput";
+            textBoxOutput.ReadOnly = true;
+            textBoxOutput.Size = new Size(274, 356);
+            textBoxOutput.TabIndex = 3;
+            // 
+            // buttonAdd
+            // 
+            buttonAdd.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            buttonAdd.Location = new Point(311, 361);
+            buttonAdd.Name = "buttonAdd";
+            buttonAdd.Size = new Size(200, 65);
+            buttonAdd.TabIndex = 4;
+            buttonAdd.Text = "Добавить элемент";
+            buttonAdd.UseVisualStyleBackColor = true;
+            buttonAdd.Click += button1_Click;
+            // 
+            // textBoxPut1
+            // 
+            textBoxPut1.Location = new Point(0, 120);
+            textBoxPut1.Name = "textBoxPut1";
+            textBoxPut1.Size = new Size(340, 23);
+            textBoxPut1.TabIndex = 5;
+            // 
+            // textBoxPut2
+            // 
+            textBoxPut2.Location = new Point(0, 203);
+            textBoxPut2.Name = "textBoxPut2";
+            textBoxPut2.Size = new Size(340, 23);
+            textBoxPut2.TabIndex = 6;
+            // 
+            // textBoxPut3
+            // 
+            textBoxPut3.Location = new Point(0, 279);
+            textBoxPut3.Name = "textBoxPut3";
+            textBoxPut3.Size = new Size(340, 23);
+            textBoxPut3.TabIndex = 7;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(0, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(128, 15);
+            label1.TabIndex = 8;
+            label1.Text = "Введите имя студента:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(0, 102);
+            label2.Name = "label2";
+            label2.Size = new Size(144, 15);
+            label2.TabIndex = 9;
+            label2.Text = "Введите группу студента:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(1, 185);
+            label3.Name = "label3";
+            label3.Size = new Size(148, 15);
+            label3.TabIndex = 10;
+            label3.Text = "Введите возраст студента:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(1, 261);
+            label4.Name = "label4";
+            label4.Size = new Size(196, 15);
+            label4.TabIndex = 11;
+            label4.Text = "Введите город рождения студента:";
+            label4.Click += label4_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label5.Location = new Point(12, 9);
+            label5.Name = "label5";
+            label5.Size = new Size(114, 37);
+            label5.TabIndex = 12;
+            label5.Text = "Студент";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(30, 52);
+            label6.Name = "label6";
+            label6.Size = new Size(196, 15);
+            label6.TabIndex = 13;
+            label6.Text = "Сохранённые карточки студентов:";
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = Color.Transparent;
+            groupBox1.BackgroundImageLayout = ImageLayout.None;
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(textBoxPut3);
+            groupBox1.Controls.Add(textBoxPut2);
+            groupBox1.Controls.Add(textBoxPut1);
+            groupBox1.Controls.Add(textBoxPut0);
+            groupBox1.Location = new Point(310, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(379, 338);
+            groupBox1.TabIndex = 14;
+            groupBox1.TabStop = false;
+            // 
+            // errorProvider1
+            // 
+            errorProvider1.ContainerControl = this;
+            // 
+            // errorProvider2
+            // 
+            errorProvider2.ContainerControl = this;
+            // 
+            // errorProvider3
+            // 
+            errorProvider3.ContainerControl = this;
+            // 
+            // errorProvider4
+            // 
+            errorProvider4.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(689, 450);
+            Controls.Add(groupBox1);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(buttonAdd);
+            Controls.Add(textBoxOutput);
+            Controls.Add(buttonClear);
+            Name = "Form1";
+            Text = "Лабараторная работа 4. Задание 2";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)errorProvider2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)errorProvider3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)errorProvider4).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private TextBox textBoxPut0;
+        private Button buttonClear;
+        private TextBox textBoxOutput;
+        private Button buttonAdd;
+        private TextBox textBoxPut1;
+        private TextBox textBoxPut2;
+        private TextBox textBoxPut3;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private GroupBox groupBox1;
+        private ErrorProvider errorProvider1;
+        private ErrorProvider errorProvider2;
+        private ErrorProvider errorProvider3;
+        private ErrorProvider errorProvider4;
+    }
+}
